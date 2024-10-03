bfe530e72a9c4567cf7548a296cd1050a323267c
Jeremy Brenner <brennerj@objectcomputing.com>
2024-10-03 10:38:16 -0500

    add red for x shape and refactor things down a bit

--- futurama/led-matrix-animation/led-matrix-animation.ino
+++ futurama/led-matrix-animation/led-matrix-animation.ino
@@ -10,9 +10,9 @@
 #define SLEEP_MAX_TIME 5000
 
 int lights[NUM_LIGHTS][LEDS_PER_LIGHT] = { 
-  { 0, 1, 14, 15 },
-  { 3, 4, 11, 12 },
-  { 6, 7, 8, 9 },
+  {  0,  1, 14, 15 },
+  {  3,  4, 11, 12 },
+  {  6,  7,  8,  9 },
   { 30, 31, 32, 33 },
   { 27, 28, 35, 36 },
   { 24, 25, 38, 39 },
@@ -25,6 +25,7 @@ CRGB leds[NUM_LEDS];
 
 CRGB onColor = CRGB::LimeGreen;
 CRGB offColor = CRGB::Black;
+CRGB redColor = CRGB::Red;
 
 long currentState = 0;
 long lastState = 0;
@@ -40,7 +41,7 @@ void loop() {
   
   for (int step = 1; step <= ANIMATION_STEPS; step++) {
     for (int lightIdx = 0; lightIdx < NUM_LIGHTS; lightIdx++) {
-      CRGB color = getLightColor(lightIdx,step);
+      CRGB color = blendColor(lightIdx, step);
       updateLight(lightIdx,color);
     }
     FastLED.show(); 
@@ -50,38 +51,24 @@ void loop() {
   delay(sleepTime);
 }
 
-CRGB getLightColor(int lightIdx, int step) {
-  bool isOn = lightIsOn(lightIdx);
-  bool wasOn = lightWasOn(lightIdx);
-  if(isOn && wasOn) {
-    return onColor;
-  }
-  if(!isOn && !wasOn) {
-    return offColor;
-  }
-  return isOn ? 
-    fadeIn(step): 
-    fadeOut(step);
-}
-
 void updateLight(int lightIdx, CRGB color) {
   for (int ledIdx = 0; ledIdx < LEDS_PER_LIGHT; ledIdx++) {
     leds[lights[lightIdx][ledIdx]] = color;
   }
 }
 
-bool lightIsOn(int lightIdx) {
-  return ((currentState >> lightIdx) & 1) == 1;    
+bool lightIsOn(int state, int lightIdx) {
+  return ((state >> lightIdx) & 1) == 1;    
 }
 
-bool lightWasOn(int lightIdx) {
-  return ((lastState >> lightIdx) & 1) == 1;    
+CRGB getColor(int state, int lightIdx) {
+  if(lightIsOn(state, lightIdx)) {
+    return state == 341 ? redColor : onColor;
+  }else{
+    return offColor;
+  }
 }
 
-CRGB fadeIn(int step) {
-  return blend(offColor, onColor, (double)255/ANIMATION_STEPS * step); 
-}
-
-CRGB fadeOut(int step) {
-  return blend(onColor, offColor, (double)255/ANIMATION_STEPS * step); 
+CRGB blendColor(int lightIdx, int step) {
+  return blend(getColor(lastState,lightIdx), getColor(currentState,lightIdx), (double)255/ANIMATION_STEPS * step); 
 }
